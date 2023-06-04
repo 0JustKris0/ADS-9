ac0495a1d9c24a212e15b4a7ec9f357cbe6631ee
0JustKris0 <125268052+0JustKris0@users.noreply.github.com>
2023-06-04 23:34:51 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,49 +1,74 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-#include <algorithm>
-template <typename T>
+template<typename T>
 class BST {
+ public:
+  BST() :root(nullptr) {}
+  void AddValue(const T& value) {
+    root = addNode(root, value);
+  }
+  int search(const T& value) {
+    return searchNode(root, value);
+  }
+  int depth() {
+    return getDepth(root) - 1;
+  }
  private:
   struct Node {
+    Node() :count(0), left(nullptr), right(nullptr) {}
     T value;
-    Node *right;
-    Node* left;
     int count;
-    explicit Node(T value)
-        : value(value), count(1), left(nullptr), right(nullptr) {}
+    Node* left;
+    Node* right;
   };
- public:
   Node* root;
-  BST() : root(nullptr) {}
-  int search(T value) { return searchNode(root, value); }
-  int searchNode(Node* root, T value) {
-    if (!root) return 0;
-    if (root->value == value) return root->count;
-    if (value < root->value)
-      return searchNode(root->left, value);
-    else
-      return searchNode(root->right, value);
-  }
-  int depth() { return getDepth(root) - 1; }
-  int getDepth(Node* root) {
-    if (root)
-      return std::max(getDepth(root->left), getDepth(root->right)) + 1;
-    else
-      return 0;
-  }
-  void add(T value) { root = insertWord(root, value); }
-  Node* insertWord(Node* ptr, T word) {
-    if (ptr == nullptr) {
-      ptr = new Node(word);
-    } else if (ptr->value == word) {
-      ptr->count++;
-    } else if (ptr->value > word) {
-      ptr->left = insertWord(ptr->left, word);
-    } else if (ptr->value < word) {
-      ptr->right = insertWord(ptr->right, word);
+
+  Node* addNode(Node* root, const T& value) {
+    if (!root) {
+    if (root == nullptr) {
+      root = new Node;
+      root->value = value;
+      root->left = nullptr;
+      root->right = nullptr;
+      root->count = 1;
+    } else if (root->value > value) {
+      root->left = addNode(root->left, value);
+    } else if (root->value < value) {
+      root->right = addNode(root->right, value);
+    } else if (root->value < value) {
+      root->left = addNode(root->left, value);
+    } else {
+      root->count++;
     }
-    return ptr;
+    return(root);
+    return root;
+  }
+
+  int searchNode(Node* root, const T& value) {
+    if (!root) {
+    if (root == nullptr) {
+      return 0;
+    } else if (root->value < value) {
+      return searchNode(root->left, value);
+    } else if (root->value > value) {
+      return searchNode(root->right, value);
+    } else if (root->value == value) {
+      return root->count;
+    }
+    return 0;
+  }
+  int getDepth(Node* root) {
+    if (!root) {
+      return 0;
+    }
+    int rightTree = getDepth(root->right);
+    int leftTree = getDepth(root->left);
+    if (rightTree > leftTree) {
+      return rightTree + 1;
+    } else {
+      return leftTree + 1;
+    }
   }
 };
 #endif  // INCLUDE_BST_H_
